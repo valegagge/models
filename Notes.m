5d5db0fdb0b760f58d50156f171cc820e6db723e
Valentina Gaggero <valentina.gaggero@iit.it>
2021-10-20 00:16:17 +0200

    Create Note.m

---
+++ Notes.m
@@ -0,0 +1,6 @@
+Some notes
+
+In order to add a new paramter:
+ 1. add the paramyer in the mask
+ 2. add the paramter in the list of parameter (right clock on sfunction -> paramter menu)
+ 3. adjust the numebr farameter in ssSetNumSFcnParams function
